90d461f9b7aaf2a75e9cf0a1a9b665888cb0b01e
Jessica Brantez <jesstez@iu.edu>
2024-11-19 15:03:27 -0500

    fix forward stepwise

--- final-proj.R
+++ final-proj.R
@@ -10,11 +10,9 @@ colnames(simdata) <- c("X1", "X2", "X3", "X4", "X5",
                        "X11", "X12", "X13", "X14", "X15")
 #Create outcome column
 Y <- rnorm(n)
-simdata <- cbind(simdata, Y)
+simdata <- cbind(Y, simdata)
 #Check
 head(simdata)
-cor_matrix <- cor(simdata)
-cor_matrix
 
 # 2. Forward Step-wise
 null <- lm(Y ~ 1, data = simdata)
@@ -23,23 +21,27 @@ n_x <- ncol(simdata) - 1
 remain_vars <- vars
 oldvars <- "1"
 modlist <- list()
+selected_models <- list()
 for(k in 1:n_x){
   rvec <- numeric(length(remain_vars))
   for(i in 1:length(remain_vars)){
-    modlist[[i]] <- lm(Y ~ oldvars + remain_vars[i])
-    rvec[i] <- modlist[[i]]$r.squared
+      var_formula <- paste("Y ~", paste(oldvars, 
+                         remain_vars[i], 
+                         sep = " + ", 
+                         collapse = " + "))
+    modlist[[i]] <- lm(as.formula(var_formula), data=simdata)
+    rvec[i] <- summary(modlist[[i]])$r.squared
   }
   bestvar_n <- which.max(rvec)
-  remain_vars <- remain_vars[-bestvar_n]
   mod <- modlist[[bestvar_n]]
-  selected_models[[k]] <- mod
   oldvars <- paste(oldvars, remain_vars[bestvar_n], sep = "+")
+  remain_vars <- remain_vars[-bestvar_n]
+  selected_models[[k]] <- mod
 }
-selected_BIC <- sapply(selected_models, BIC())
-maxBIC_i <- which.max(selected_BIC)
-bestmod <- selected_models[maxBIC_i]
-bestmod
-
+selected_BIC <- sapply(selected_models, function(x) BIC(x))
+minBIC_i <- which.min(selected_BIC)
+bestmod <- summary(selected_models[[minBIC_i]])
+bestmod$coefficients[-1, 4]
 
 
 
