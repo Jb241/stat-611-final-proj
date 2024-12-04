7d447963c86e31b8ef97c8b10630123f5e037723
Jessica Brantez <jesstez@iu.edu>
2024-12-04 14:12:54 -0500

    shorten implementation file

--- final-proj-implement.R
+++ final-proj-implement.R
@@ -8,6 +8,5 @@ source("final-proj-fn.R")
 set.seed(55)
 data <- simulate_data(5000, 30)
 best_model <- forward_stepwise(data)
-hypothesis_test(best_model)
 which_xs_sig <- hypothesis_test(best_model)
 which_xs_sig
